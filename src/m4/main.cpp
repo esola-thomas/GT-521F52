d3ad41c3934019350b74ff699836a92f7acd68ab
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-15 23:21:55 -0400

    Filter src from different platforms

--- src/m4/main.cpp
+++ src/m4/main.cpp
@@ -6,10 +6,15 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(LEDB, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LEDB, HIGH);
+  delay(1);
+  digitalWrite(LEDB, LOW);
+  delay(1);
 }
 
 // put function definitions here:
